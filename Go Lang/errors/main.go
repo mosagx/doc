c1f3036c0da754fee93fa45ee8d8f5d130ce5f1a
陈胜 <chensheng@mianchao-inc.com>
2018-09-11 17:39:28 +0800

    2018年9月11日

--- Go Lang/errors/main.go
+++ Go Lang/errors/main.go
@@ -17,10 +17,23 @@ func (de *DivideError) Error() string {
 }
 func Divide(varDividee int, varDivider int) (result int, errorMsg string) {
 	if varDividee == 0 {
-
+		dData := DivideError{
+			dividee: varDividee,
+			divider: varDivider,
+		}
+		errorMsg = dData.Error()
+		return
+	} else {
+		return varDividee / varDivider, ""
 	}
 }
 
 func main() {
+	if result, errorMsg := Divide(100, 10); errorMsg == "" {
+		fmt.Println("100/10 = ", result)
+	}
 
+	if _, errorMsg := Divide(100, 0); errorMsg != "" {
+		fmt.Println("errorMsg is:", errorMsg)
+	}
 }
